32dd47368d0facefd800f39863a88884ef8d464b
Nayan <dreamerboynayan@gmail.com>
2025-05-08 06:36:52 +0600

    String Practice

---
+++ string/string.js
@@ -0,0 +1,20 @@
+const name = "Nayan Mia";
+console.log(name); // Nayan Mia
+
+const address = "Faridpur";
+console.log(address); // Faridpur
+
+const mituReads = "Chattogram University";
+console.log(mituReads); // Chattogram University 
+
+const fan = "Shayla Islam";
+console.log(fan); // Shayla Islam 
+
+const DramafTheYear = "Mohanogor Season 1 & 2";
+console.log(DramafTheYear); // Mohanogor Season 1 & 2
+
+const hobby = "Reading Books";
+console.log(hobby); // Reading Books
+
+const color = "Black";
+console.log(color); // Black
