481144a064a015d23d644f22bebbdd52301c59b4
Nayan <dreamerboynayan@gmail.com>
2025-05-08 07:20:39 +0600

    Math Operator

---
+++ Math Operation/math.js
@@ -0,0 +1,63 @@
+let income = 100;
+let cost = 40;
+
+const nowHave = income - cost;
+console.log(nowHave); // 60
+
+const havePensil = 10;
+const useOneTime = 2;
+
+const howLongUse = 10 / 2;
+console.log(howLongUse); // 5
+
+const haveMoney = 60;
+const costOneTime = 15;
+
+const howLongUses = 60 / 15 ;
+const afterUsesReminder = 60 % 15;
+
+console.log(howLongUses, afterUsesReminder); // 4 ,0
+
+const str1 = "Hello";
+const str2 = "World";
+
+const strPair = str1 + str2;
+console.log(strPair); // HelloWorld
+
+const buyRice = 153;
+const buyLentils = 261;
+
+const totalBuy = buyRice + buyLentils;
+console.log(totalBuy); // 414
+
+const haveTaka = 500;
+const usePerTime = 75;
+
+const howLongUsing = haveTaka / usePerTime ;
+const afterUsingReminder = haveTaka % usePerTime; 
+
+console.log(howLongUsing, afterUsingReminder); // 6.666666, 50
+
+console.log(8 / 3); // 2.66666
+console.log(8 % 3); // 2
+console.log(50 % 9); // 5
+
+const strr1 = "Bangla";
+const strr2 = "desh";
+
+const strrResult = strr1 + strr2;
+console.log(strrResult); // Bangladesh
+
+
+const haveRice = 480 ;
+const eatPerDay = 4;
+
+const totalDay = haveRice / eatPerDay;
+console.log(totalDay); // 120 
+
+const totalDays = 120;
+const oneMonth = 30;
+
+const totalMonths = totalDays / oneMonth;
+console.log(totalMonths); // 4
+
