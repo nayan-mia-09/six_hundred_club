f7b00bcff16f9f3737c4ab23a7c3fd418103f2d1
Nayan <dreamerboynayan@gmail.com>
2025-05-08 06:19:38 +0600

    Variable Practiace 1

---
+++ Variable/variable.js
@@ -0,0 +1,22 @@
+const birthDate = 2003;
+console.log(birthDate); // 2003
+
+const siblings = 2;
+console.log(siblings); // 2
+
+let students = 100;
+console.log(students); // 100
+
+const marks = 85;
+console.log(marks); // 85
+
+const rooms = 3;
+console.log(rooms); // 3
+
+const hours = 8;
+console.log(hours); // 8
+
+const phoneDigit = 0+1+7+7+8+1+2+3+0+9+6+5;
+const result = phoneDigit;
+
+console.log(result); // 49
