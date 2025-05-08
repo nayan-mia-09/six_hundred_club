348761df1010aef5144b60eb9f6ace6aa3c4bbfc
Nayan <dreamerboynayan@gmail.com>
2025-05-08 06:28:20 +0600

    Console Practice

---
+++ Variable/console.js
@@ -0,0 +1,20 @@
+let age = 22;
+console.log(age); // 22
+
+let booksToRead = 10;
+console.log(booksToRead); //10
+
+let wakeUpTime = 5;
+console.log(wakeUpTime); // 5  
+
+let wastedTime = 2;
+console.log(wastedTime); // 2
+
+let borrow = 60000;
+console.log(borrow); // 60000
+
+let goals = 4;
+console.log(goals); // 4
+
+let pillow = 1;
+console.log(pillow); // 1
