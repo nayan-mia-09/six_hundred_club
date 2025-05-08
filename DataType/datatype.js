02c17a4f94ab40f4d72f8add262f621ed2cf2f9a
Nayan <dreamerboynayan@gmail.com>
2025-05-08 07:38:46 +0600

    Data Type

---
+++ DataType/datatype.js
@@ -0,0 +1,17 @@
+const price = 555;
+console.log(typeof price); // number
+
+const learning = true;
+console.log(typeof learning); // boolean
+
+const megaCity = "Dhaka";
+console.log(typeof megaCity); // string
+
+const temperature = 102.5;
+console.log(typeof temperature); // number
+
+const isHungry = true;
+console.log(typeof isHungry); // boolean 
+
+const isRich = true;
+console.log(typeof isRich); // boolean
