e01025000e819ca816eb8f7f7ca3335a30f78c36
Nayan <dreamerboynayan@gmail.com>
2025-05-08 10:29:02 +0600

    Variable Naming

---
+++ Variable/variableNaming.js
@@ -0,0 +1,29 @@
+let favFood = "Briyani"; // camelCase
+
+console.log(favFood);// Briyani
+
+// Case-Sensitive 
+
+let city = "Dhaka";
+
+let City = "Chittagong";
+
+console.log(city, City); // Dhaka Chittagong
+
+
+let userName = "Johan";
+
+let username = "Jovan";
+
+console.log(userName, username); // Johan Jovan
+
+let myAddress = "Sadarpur, Faridpur"; // camelCase
+
+console.log(myAddress); // Sadarpur, Faridpur
+
+// let super = 25; In TypeScript (and JavaScript), the super keyword is used in derived classes to access and call functions on an object's parent class. This keyword must be used correctly to maintain proper inheritance (the mechanism by which one class can inherit properties and methods from another class).
+
+
+// let class = "Honours 3rd year" Variable declaration not allowed at this location.
+//variable names cannot start with a digit in most programming languages,
+// let 2ndPosition = "Rahim" This would cause a syntax error.
