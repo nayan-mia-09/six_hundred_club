b8405df0bedd1d7b8b7bea9242449d8ebfb9d3e9
Nayan <dreamerboynayan@gmail.com>
2025-05-08 10:13:21 +0600

    Commenting on JavaScript

---
+++ Commenting/comment.js
@@ -0,0 +1,46 @@
+
+// Football Team's A 
+const score = 4;
+console.log(score); // 4
+
+// Earth Radius
+// The equatorial radius is often used to compare Earth with other planets.
+// The Earth's equatorial radius a, or semi-major axis,
+// is the distance from its center to the equator and equals
+
+const radius = 6378;
+console.log(radius); // 6378
+
+ 
+/*
+ My Favriout Dress collection here
+ My favourite dress is the Kurti, which is a classic long frock. It is typically worn during events or family gatherings. Kurtis is easy to wear, allows me to move freely and easily, and always appears sober.*/
+const dress = 6;
+
+console.log(dress); //6
+
+// I'm Completed my task that's why he give me my reward
+
+const giftMoney = 100;
+
+console.log(giftMoney); //100
+
+// Today I write code 7 houres about JavaScript 
+// At first I'm watching JavaScript Tutorial
+// Today I learn Variable, String, DataType
+// Then I write my code 
+
+const studyTimes = 7;
+console.log(studyTimes); // 7
+
+// 1000tk Send money to Mitu 
+// Send money cost 20tk 
+
+const sendMoney = 1000;
+console.log(sendMoney); // 1000
+
+// Product price and delivery charge included
+
+const pay = 1280;
+console.log(pay); // 1280
+ 
