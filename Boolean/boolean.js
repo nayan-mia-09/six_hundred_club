cd60f31937167fdd29bd795d872a65eb1ccf6968
Nayan <dreamerboynayan@gmail.com>
2025-05-08 06:46:01 +0600

    Boolean Practice

---
+++ Boolean/boolean.js
@@ -0,0 +1,23 @@
+const friendHappy = false;
+console.log(friendHappy); // false
+
+const crushMessage = false;
+console.log(crushMessage); // false
+
+const celebrating = true;
+console.log(celebrating); //true
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+
+console.log(isHappy, hasMoney, isHungry); // true false true
+
+const isStudyDone = true;
+console.log(isStudyDone); // true
+
+const isPhoneCharged = false;
+console.log(isPhoneCharged); // false 
+
+const isDinnerDone = true;
+console.log(isDinnerDone); // true
