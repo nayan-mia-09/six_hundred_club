ca85f082a9c9680c0ad688607b715e16a576faf7
Nayan <dreamerboynayan@gmail.com>
2025-05-08 07:47:53 +0600

    Change Variable Value

---
+++ Variable/changevalue.js
@@ -0,0 +1,36 @@
+let age = 21;
+console.log(age); // 21
+age = 22;
+console.log(age); // 22
+
+let pocketMoney = 500;
+
+pocketMoney = 150;
+console.log(pocketMoney); // 150
+
+let studyTime = 8 ;
+
+studyTime = 2;
+console.log(studyTime); // 2
+
+let friendCount = 3;
+
+friendCount = 1;
+console.log(friendCount); // 1
+
+let phonePrice = 25000;
+
+phonePrice = 12000;
+console.log(phonePrice); // 12000
+
+let chaiCount = 40;
+
+chaiCount = 38;
+chaiCount = 32;
+console.log(chaiCount); // 32
+
+let playTime = 1;
+
+playTime = 0;
+
+console.log(playTime); // 0
