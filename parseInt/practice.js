40fa96ad578ef475592cf3abcc25f26c8372e8dc
Nayan <dreamerboynayan@gmail.com>
2025-05-09 15:10:45 +0600

    ParseInt and ParseFloat Practice

---
+++ parseInt/practice.js
@@ -0,0 +1,47 @@
+
+// Practice 1
+
+const num = "20";
+const Num = parseInt(num);
+const sum = Num + 10;
+console.log(sum); // 30
+
+// Practice 2 
+
+const value = "3.14159";
+const valueConverToNum = parseInt(value);
+console.log(valueConverToNum); // 3
+
+// Practice 3
+
+const pemika = "premikBoy";
+const pemikaConvertToNum = parseInt(pemika);
+console.log(pemikaConvertToNum); // NaN
+
+// Practice = 4
+
+const num1 = 3.456;
+const num2 = 2.789;
+const totalnum = num1 + num2 ;
+console.log(totalnum.toFixed(2)); //6.25
+
+// Practice 5 
+
+const num3 = parseInt("56.78");
+const num4 = parseInt("12.34");
+const sumTwo = num3 + num4;
+console.log(sumTwo); //  68
+
+// Practice 6 
+
+const num5 = parseFloat("10.5678");
+const num6 = parseFloat("5.4321");
+
+const Sum2 = num5 + num6;
+console.log(Sum2.toFixed(1)); // 16.0
+
+// Practice 7 
+
+const round = parseFloat("42.45689754");
+console.log(round.toFixed(3)); // 42.457
+
