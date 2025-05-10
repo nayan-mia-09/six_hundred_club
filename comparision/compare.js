fee9fa8e2bb70b9ccd053865a71ef5bd33ccb24e
Nayan <dreamerboynayan@gmail.com>
2025-05-10 07:23:47 +0600

    Comparision Practice 1

---
+++ comparision/compare.js
@@ -0,0 +1,51 @@
+// compare 1
+
+const ratulEatSanduich = 7;
+const rafiEatSanduich = 5;
+
+const result = ratulEatSanduich > rafiEatSanduich;
+console.log(result); // true
+
+// compare 2
+
+const clsSevenStudent = 45;
+const clsEightStudent = 50;
+
+const Result = clsEightStudent > clsSevenStudent;
+console.log(Result); // true
+
+// compare 3
+
+const myMarks = 75;
+const friendMarks = 75;
+const isEqual = myMarks === friendMarks ;
+console.log(isEqual); // true
+
+// compare 4
+
+const target = 100;
+const savings = 95;
+
+const isTargetFill = target <= savings; 
+console.log(isTargetFill); // false
+
+// compare 5
+
+const num1 = 7;
+const num2 = 10;
+
+const numIsEqual = num1 === num2;
+console.log(numIsEqual); // false
+
+// compare 6
+
+console.log(20 <= 15); // false
+
+// compare 7
+
+const myResult = 45;
+const friendResult = 95;
+
+const isResultEqual = friendResult < myResult;
+console.log(isResultEqual); // false
+
