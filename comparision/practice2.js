fd85f4f3278920a1ef9d5ec67cc2efe9a1e7f371
Nayan <dreamerboynayan@gmail.com>
2025-05-10 07:45:24 +0600

    Comparision Practice 2

---
+++ comparision/practice2.js
@@ -0,0 +1,72 @@
+// compare 1
+
+const ratulAge = 10;
+const myAge = 15;
+
+const ageCompare = myAge >= ratulAge;
+
+console.log(ageCompare); // true
+
+// compare 2 
+
+const passMark = 33;
+const myMark = 45;
+
+const isPassed = passMark <= myMark;
+
+console.log(isPassed); // true
+
+// compare 3
+
+const jamburaPrice = 50;
+const kodbellPrice = 20;
+
+const priceIsLess = jamburaPrice < kodbellPrice;
+
+console.log(priceIsLess); // false
+
+// compare 4
+
+const babuReads = 10;
+const meRead = 10;
+
+const readEquals = babuReads === meRead;
+
+console.log(readEquals); // true
+
+// compare 5
+
+const myBuiscits = 15;
+const friendBuiscits = 20;
+ 
+const buiscitsLess = myBuiscits < friendBuiscits;
+
+console.log(buiscitsLess); // true
+
+// compare 6
+
+const madridGoal = 2;
+const barcaGoal = 3;
+
+const goalLess = madridGoal < barcaGoal;
+
+console.log(goalLess); // true
+
+// compare 7
+
+const myName = "Rahim";
+const friendName = "Karim";
+
+const isNameEqual = myName === friendName;
+
+console.log(isNameEqual); // false
+
+// compare 8
+
+const meReads = true;
+const friendReads = false;
+
+const isBothEqual = meReads === friendReads;
+
+
+console.log(isBothEqual); // false
