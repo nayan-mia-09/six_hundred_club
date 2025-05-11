bd31951648836d4fb79a49750dced92779517f9f
Nayan <dreamerboynayan@gmail.com>
2025-05-11 10:24:11 +0600

    IF Else Practice 1

---
+++ If else/practice.js
@@ -0,0 +1,52 @@
+
+// 1
+const backToHome = 5
+if(backToHome < 6){
+    console.log("Ne Biriyani Kha!");
+}else{
+    console.log("Jutar Bari Kha");
+}
+
+// 2
+
+const mobile = "Login";
+if(mobile === "Login"){
+    console.log("Welcome!");
+}else{
+    console.log("Get Lost!");
+}
+
+// 3
+
+const run = 5;
+if(run === 5){
+    console.log("Candy For You!");
+}else{
+    console.log("Tor Vuri Mota!");
+}
+
+// 4 
+
+const result = 85;
+if( result > 80 ){
+    console.log("Bike Pabi!");
+}else{
+    console.log("Vat Khwa Bondho!");
+}
+
+// 5
+
+const movieTime = 8;
+if(movieTime < 9){
+    console.log("Movie Dekhbo!");
+}else{
+    console.log("Bari Giye Ghumabo!");
+}
+
+// 6
+const temp = 30;
+if(temp >= 30){
+    console.log("AC Mood ON!");
+}else{
+    console.log("Kombol Muri Diye Ghum!");
+}
