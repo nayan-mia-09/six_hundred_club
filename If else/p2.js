a4b3339b449cfb1cd621ce45f678f3c4388031a3
Nayan <dreamerboynayan@gmail.com>
2025-05-11 11:04:26 +0600

    IF Else Practice 2

---
+++ If else/p2.js
@@ -0,0 +1,98 @@
+// 1
+
+const age = 28;
+const height = 66;
+
+if(age > 18 && height > 60){
+    console.log("Tui gari Dhakka Dibi!");
+}else{
+    console.log("Tui garite Bose Thakbi!");
+}
+
+// 2
+
+const mathResult = 80;
+const englishResult = 85;
+
+if(mathResult > 80 && englishResult > 85){
+console.log("You are eligible to admition test!");
+}else{
+    console.log("Tor Biye hobe na!");
+}
+
+// 3
+
+const result = 5;
+const familyIncome = 8000;
+
+if(result === 5 && familyIncome < 10000){
+    console.log("Tui scholarship pabi!");
+}else{
+     console.log("Tui scholarship pabi na!");
+}
+
+// 4
+
+const applicantAge = 28;
+const expreince = 4;
+
+if(applicantAge < 30 && expreince > 2){
+    console.log("Exam dite parbi");
+}else{
+    console.log("Exam dite parbi Na!");
+}
+
+// 5
+
+const haveEgg = 15;
+const haveChiken = true;
+
+if(haveEgg > 12 && haveChiken === true){
+    console.log("Dim er Korma Ranna korbo!");
+}else{
+    console.log("Ruti R PawRuti Kola Khabo!");
+}
+
+// 6
+
+const bodyTemp = 96;
+const caugh = true;
+
+if( bodyTemp > 100 && caugh === true){
+console.log("Go to Doctor's!");
+}else{
+    console.log("Katha Muri diye Ghuma!");
+}
+
+// 7
+
+const attendence = 90;
+const homeWork = true;
+
+if(attendence > 80 && homeWork === true){
+    console.log("Tui Exam dite parbi");
+}else{
+    console.log("Tui Exam dite parbi Na!");
+}
+
+// 8
+
+const electricityGone = true;
+const phoneCharge = true;
+
+if(electricityGone === true && phoneCharge === false){
+    console.log("Porte Boshbo!");
+}else{
+    console.log("Porte Boshbo Na!");
+}
+
+// 9 
+
+const shirtPrice = 900;
+const haveCoupon = true;
+
+if(shirtPrice > 1000 && haveCoupon === true){
+    console.log("You Have Discount 20%");
+}else{
+    console.log("You don't have discount 20%");
+}
